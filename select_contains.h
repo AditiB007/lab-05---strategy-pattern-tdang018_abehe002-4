6b643e10f42e305df8556acbdfa9467e44d7e165
tdang93 <aznnoodle101@hotmail.com>
2021-05-03 20:21:28 -0700

    Select_Contains works for first two calls to print_selection() in main.cpp
    * modified select_contains.h
    * modified spreadsheet.cpp

--- select_contains.h
+++ select_contains.h
@@ -19,17 +19,22 @@ public:
 
     bool select(const Spreadsheet* sheet, int row) const {
       int column = sheet->get_column_by_name(columnName);
-      std::string data = sheet->cell_data(row, column);      
-      for(int i=0; i<data.size(); ++i) {
-        for(int j=0; j<substring.size(); ++j) {
-           //std::cout << substring.at(j);
-           if(i+j > data.size()-1) { return false; }
-           if(data.at(i+j) != substring.at(j)) { continue; }
-           //std::cout << substring.at(j);
-        } // able to loop through both strings without the substring having any mismatches
+      std::string entry = sheet->cell_data(row, column);      
+      //std::cout << "data at cell(row, column): " + data << std::endl;
+      //std::cout << "row: " + std::to_string(row) << std::endl;
+      //std::cout << "column: " + std::to_string(column) << std::endl;
+      for(int i=0; i<entry.size(); ++i) { // assume entry is longer than substring, loop through it
+        //std::cout << "i: " << std::to_string(i) << " " << entry.at(i) << std::endl;
+        int count = 0;
+        for(int j=0; j<substring.size(); ++j) { // assume substring is shorter, loop through it
+           //std::cout << "j: " << std::to_string(j)  << " " << substring.at(j) << " " << std::endl;
+           if(i+j > entry.size()) { return false; }
+           if(entry.at(i+j) == substring.at(j)) { ++count; }
+        }
+        if(count == substring.size()) { return true; }
+        // able to loop through both strings without the substring having any mismatches
+        else { return false; }
       }
-      std::cout << "true" << std::endl;
-      return true;
     }
 };
 
